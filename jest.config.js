8162ea58e1e962715a67908f4b86f84df73d4e7c
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2021-06-26 22:30:04 +0200

    test: create basic jest config (#17)
    * test: create basic jest config
    
    * remove comments
    
    * Remove explicit coverageDirectory: coverage setting as its the default value
    
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>
    
    * Remove coverageProvider v8 (currently bad for TypeScript)
    
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>
    
    * Add settings we override in renovate repo
    
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>
    
    * run prettier
    
    * Rely on Jest defaults
    
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>
    
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  clearMocks: true,
+  cacheDirectory: ".cache/jest",
+  collectCoverage: true,
+  coverageDirectory: "./coverage",
+  coverageReporters: ["html", "json", "text-summary"],
+};
