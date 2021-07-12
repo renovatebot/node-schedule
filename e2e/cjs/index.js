2185e9f20012fcf15c2fc1f9badaf8826bed5b34
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2021-07-12 18:32:19 +0200

    use suggestion from viceice

--- e2e/cjs/index.js
+++ e2e/cjs/index.js
@@ -1,6 +1,6 @@
-const ns = require('node-schedule');
+const { isEOL } = require('node-schedule');
 
-if (!ns.isEOL('v0.10.48')) {
+if (!isEOL('v0.10.48')) {
   throw new Error('Expected v10 to be EOL');
 }
 
